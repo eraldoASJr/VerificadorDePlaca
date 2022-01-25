4c9c4d48c598c40f639a0c077649c09ae7a98495
Eraldo Almeida Santos Junior <62157365+eraldoASJr@users.noreply.github.com>
2022-01-25 01:45:50 -0300

    Add files via upload

---
+++ ProjetoPlacas/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ProjetoPlacas
+{
+    partial class FrmRodizio
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFinalPlaca = new System.Windows.Forms.TextBox();
+            this.btnVerifica = new System.Windows.Forms.Button();
+            this.lblMensagem = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFinalPlaca
+            // 
+            this.txtFinalPlaca.Location = new System.Drawing.Point(301, 64);
+            this.txtFinalPlaca.Name = "txtFinalPlaca";
+            this.txtFinalPlaca.Size = new System.Drawing.Size(95, 38);
+            this.txtFinalPlaca.TabIndex = 0;
+            // 
+            // btnVerifica
+            // 
+            this.btnVerifica.Location = new System.Drawing.Point(423, 64);
+            this.btnVerifica.Name = "btnVerifica";
+            this.btnVerifica.Size = new System.Drawing.Size(190, 61);
+            this.btnVerifica.TabIndex = 1;
+            this.btnVerifica.Text = "Verificar";
+            this.btnVerifica.UseVisualStyleBackColor = true;
+            this.btnVerifica.Click += new System.EventHandler(this.btnVerifica_Click);
+            // 
+            // lblMensagem
+            // 
+            this.lblMensagem.AutoSize = true;
+            this.lblMensagem.Location = new System.Drawing.Point(167, 152);
+            this.lblMensagem.Name = "lblMensagem";
+            this.lblMensagem.Size = new System.Drawing.Size(24, 31);
+            this.lblMensagem.TabIndex = 2;
+            this.lblMensagem.Text = "-";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Final da Placa:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(214, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(286, 62);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Horário: 7h às 10h e \r\n               17h às 20h";
+            // 
+            // FrmRodizio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMensagem);
+            this.Controls.Add(this.btnVerifica);
+            this.Controls.Add(this.txtFinalPlaca);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "FrmRodizio";
+            this.Text = "Rodizio de Veículos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFinalPlaca;
+        private System.Windows.Forms.Button btnVerifica;
+        private System.Windows.Forms.Label lblMensagem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ProjetoPlacas/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoPlacas
+{
+    public partial class FrmRodizio : Form
+    {
+        public FrmRodizio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVerifica_Click(object sender, EventArgs e)
+        {
+            int varFinalPlaca;
+
+            varFinalPlaca = Convert.ToInt16(txtFinalPlaca.Text);
+
+          /*  if (varFinalPlaca == 1 || varFinalPlaca == 2)
+                lblMensagem.Text = " Proibido Segunda-Feira";
+            else if (varFinalPlaca == 3 || varFinalPlaca == 4)
+                lblMensagem.Text = " Proibido Terça-Feira";
+            else if (varFinalPlaca == 5 || varFinalPlaca == 6)
+                lblMensagem.Text = " Proibido Quarta-Feira";
+            else if (varFinalPlaca == 7 || varFinalPlaca == 8)
+                lblMensagem.Text = " Proibido Quinta-Feira";
+            else if (varFinalPlaca == 9 || varFinalPlaca == 0)
+                lblMensagem.Text = " Proibido Sexta-Feira";
+          */
+
+            switch (varFinalPlaca)
+            {
+                case 1: lblMensagem.Text = " Proibido Segunda-Feira e Licenciar até ABRIL"; break;
+
+                case 2: lblMensagem.Text = " Proibido Segunda-Feira e Licenciar até MAIO"; break;
+
+                case 3: lblMensagem.Text = " Proibido Terça-Feira e Licenciar até JUNHO"; break;
+
+                case 4: lblMensagem.Text = " Proibido Terça-Feira e LICenciar atpe JULHO"; break;
+
+                case 5:
+                case 6: lblMensagem.Text = " Proibido Quarta-Feira e LIcenciar até AGOSTO"; break;
+
+                case 7: lblMensagem.Text = " Proibido Quinta-Feira e Licenciar até SETEMBRO"; break;
+
+                case 8: lblMensagem.Text = " Proibido Quinta-Feira e Licencair até OUTUBRO"; break;
+                  
+                case 9: lblMensagem.Text = " Proibido Sexta-Feira e Licenciar até NOVMBRO"; break;
+
+                case 0: lblMensagem.Text = " Proibido Sexta-Feira e Liceniar até DEZEMBRO"; break;
+
+                default :  lblMensagem.Text = " Erro....." ;                 
+                    break;
+
+            }
+
+
+        }
+    }
+}
